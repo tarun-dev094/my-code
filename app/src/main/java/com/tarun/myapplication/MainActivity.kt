7164ecf413442f48520333083e7b1972b56ecb07
tarun-dev094 <59257094+tarun-dev094@users.noreply.github.com>
2020-04-08 23:16:58 +0530

    Update MainActivity.kt
    sd

--- app/src/main/java/com/tarun/myapplication/MainActivity.kt
+++ app/src/main/java/com/tarun/myapplication/MainActivity.kt
@@ -8,5 +8,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var a=0
+
+        var b=2
+        var e=if(a>b)a else b
+        println("Max=> $e")
     }
 }
